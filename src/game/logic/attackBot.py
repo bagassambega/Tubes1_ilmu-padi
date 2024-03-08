cb1cb70b69735945f4dbb078ab5237e2ea310bf9
Bagas <bagassambega@gmail.com>
2024-03-08 23:27:28 +0700

    feat: initialization of teleporter and chase function

--- src/game/logic/attackBot.py
+++ src/game/logic/attackBot.py
@@ -1,32 +1,22 @@
-import random
 from typing import Optional
 from typing import List
 from game.logic.base import BaseLogic
 from game.models import GameObject, Board, Position
-from ..util import get_direction
 
-'''
-Identifier inside models.py:
-board.diamonds: diamond
-board.bots: bots
-
-Identifier inside gameEngine repo (src/backend/src/gameengine/gameobjects):
-Teleporter: TeleportGameObject
-Red Button: DiamondButtonGameObject
-'''
 
 class attackBot(BaseLogic):
-    step = 0
+    #! INITIALIZATION
     def __init__(self):
         self.directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
         self.goal_position: Optional[Position] = None
         self.current_direction = 0
-    
-    def getTeleporter(self, board_bot: GameObject, board: Board):
-        return [t for t in board.game_objects if t.type == ""]        
+        self.chaseSteps = 0
+        self.isPortal = False
 
+
+    #! DIAMOND SECTION
+    # fungsi mengambil lokasi kumpulan diamond yang ada di sekitar base dalam bentuk list
     def diamondsaroundbase(self, board_bot: GameObject, board:Board):
-        # fungsi mengambil lokasi kumpulan diamond yang ada di sekitar base dalam bentuk list
         props = board_bot.properties
         diamonds = board.diamonds
         
@@ -37,57 +27,103 @@ class attackBot(BaseLogic):
         
         return diamond_loc
     
+    # Prioritasin diamond merah jika salah satu lebih dekat ke diamond merah
+    def prioritizereddiamond(self, board_bot: GameObject, board: Board):
+        current_position = board_bot.position
+
+        all_diamonds = board.diamonds
+        red_diamonds = [diamond for diamond in all_diamonds if diamond.properties.points == 2]
+        
+        if not red_diamonds:
+            return
+        
+        closest_red_diamond = min(red_diamonds, key=lambda diamond: abs(diamond.position.x - current_position.x) + abs(diamond.position.y - current_position.y))        
+        closest_normal_diamond = min(all_diamonds, key=lambda diamond: abs(diamond.position.x - current_position.x) + abs(diamond.position.y - current_position.y))
+        
+        # Ambil diamond merah jika letaknya sangat dekat dibandingkan dengan diamond biasa
+        if abs(closest_red_diamond.position.x - current_position.x) + abs(closest_red_diamond.position.y - current_position.y) < abs(closest_normal_diamond.position.x - current_position.x) + abs(closest_normal_diamond.position.y - current_position.y):
+            self.goal_position = closest_red_diamond.position
+        else:
+            self.goal_position = closest_normal_diamond.position
+    
+    # memeriksa apakah lokasi bot kita lagi di sekitar base
     def botaroundbase(self, board_bot: GameObject):
-        # memeriksa apakah lokasi bot lagi di sekitar base
         props = board_bot.properties
         current_position = board_bot.position
         return ((props.base.x-3) <= current_position.x <= (props.base.x+3) and (props.base.y-3) <= current_position.y <= (props.base.y+3))
     
+    # mencari diamond yang paling dekat base dengan posisi bot
     def closestdiamondbase(self, board_bot: GameObject, diamonds: List[Position]):
-        # mencari diamond yang paling dekat base dengan posisi bot
         current_position = board_bot.position
         closest_diamond = min(diamonds, key=lambda diamond: abs(diamond.x - current_position.x) + abs(diamond.y - current_position.y))
         return closest_diamond
-    
+
+    # cek apakah ada diamond sekitar base
     def cekdiamondbase(self, board_bot: GameObject, board:Board):
-        # cek apakah ada diamond sekitar base
         ada = False
         diamonds = board.diamonds
         
         # lokasi diamonds sekitar base
         for diamond in diamonds:
             if (board_bot.properties.base.x-2) <= diamond.position.x <= (board_bot.properties.base.x+2) and (board_bot.properties.base.y-2) <= diamond.position.y <= (board_bot.properties.base.y+2):
-               return True
+                return True
                 
         return ada
     
+    # cari diamond terdekat dengan bot (bebas di mana aja)
     def closestdiamond(self, board_bot: GameObject,board:Board):
-        # cari diamond terdekat dengan bot (bebas di mana aja)
         current_position = board_bot.position
         closest_diamond = min(board.diamonds, key=lambda diamond: (abs(diamond.position.x-current_position.x)+abs(diamond.position.y-current_position.y)))
         return closest_diamond.position
     
+    
+    # cari red diamond terdekat dengan bot (bebas di mana aja)
+    def closestreddiamond(self, board_bot: GameObject, board:Board):
+        current_position = board_bot.position
+        red_diamonds = [diamond for diamond in board.diamonds if diamond.points == 2]
+
+        if not red_diamonds:
+            return None  # Jika tidak ada red diamond, kembalikan None
+
+        closest_red_diamond = min(red_diamonds, key=lambda diamond: abs(diamond.position.x - current_position.x) + abs(diamond.position.y - current_position.y))
+        return closest_red_diamond.position
+
+    # Mencari diamond terdekat
     def closestdiamonddist(self, board_bot: GameObject,board: Board):
         # mencari jarak bot dengan diamond terdekat
         closest = self.closestdiamond(board_bot, board)
         current_position = board_bot.position
         return abs(closest.x-current_position.x)+abs(closest.y-current_position.y)
     
+    # mencari jarak bot dengan diamond merah terdekat
+    def closestreddiamonddist(self, board_bot: GameObject, board: Board):
+        current_position = board_bot.position
+        red_diamonds = [diamond for diamond in board.diamonds if diamond.properties.points == 2]
+
+        if not red_diamonds:
+            return None  # Jika tidak ada red diamond, kembalikan None
+
+        closest_red_diamond = min(red_diamonds, key=lambda diamond: abs(diamond.position.x - current_position.x) + abs(diamond.position.y - current_position.y))
+        distance = abs(closest_red_diamond.position.x - current_position.x) + abs(closest_red_diamond.position.y - current_position.y)
+        return distance
+
+
+    #! BOT SECTION
+    # Menghitung jarak (x, y) dari enemy bot ke kita
     def calculateDistanceToBots(self, board_bot: GameObject, enemy_bot: GameObject):
-        # Menghitung jarak (x, y) dari enemy bot ke kita
         return (enemy_bot.position.x - board_bot.position.x, enemy_bot.position.y - board_bot.position.y)
     
+    # Mencari semua bot yang memiliki diamonds > 3 saja dan diamonds nya lebih banyak dari kita
     def findAllBots(self, board_bot: GameObject, board: Board):
-        # Mencari semua bot yang memiliki diamonds > 3 saja dan diamonds nya lebih banyak dari kita
         listBots = []
         for bot in board.bots:
-            # Ignore bot sendiri dan bot yang ada di base sendiri
-            if (bot.id != board_bot.id and bot.position != (bot.properties.base.x, bot.properties.base.y)):
+            if (bot.id != board_bot.id):
                 if (bot.properties.diamonds > board_bot.properties.diamonds and bot.properties.diamonds >= 3):
                     listBots.append(bot)
         return listBots
     
-    def chaseBots(self, board_bot: GameObject, listBots: Optional[GameObject], board: Board):
+    # Menjadikan bot sebagai goal_position, jika jarak ke bot musuh adalah 3
+    def chaseBots(self, board_bot: GameObject, listBots: Optional[GameObject]) -> bool:
         for bot in listBots:
             dist = self.calculateDistanceToBots(board_bot, bot)
             if (dist[0] <= 3 and dist[1] <= 3):
@@ -95,6 +131,47 @@ class attackBot(BaseLogic):
                 return True
         return False
     
+    
+    #! RED BUTTON
+    # Mencari red button
+    def findRedButton(self, board: Board):
+        for item in board.game_objects:
+            if item.type == "DiamondButtonGameObject":
+                return item
+    
+    # Compare jarak closest diamond dengan red button. Lebih baik generate ulang daripada ke diamond jauh
+    # Hanya jika diamond yang ada di board sedikit (pre-checked in next_move)
+    def compareClosestDiamondToRedButton(self, board_bot: GameObject, board: Board):
+        if self.calculateDistanceRedButton(board_bot, board) < self.closestdiamonddist(board_bot, board):
+            return True
+        return False
+    
+    # Menghitung jarak ke red button 
+    def calculateDistanceRedButton(self, board_bot: GameObject, board: Board):
+        redButton = self.findRedButton(board)
+        return abs(redButton.position.x - board_bot.position.x) + abs(redButton.position.y - board_bot.position.y)
+    
+    #! TELEPORTER
+    # Mencari semua teleporter yang ada
+    def findAllTeleporter(self, board_bot: GameObject, board: Board):
+        teleporters = [item for item in board.game_objects if item.type == "TeleportGameObject"]
+        return sorted(teleporters, key=lambda tele: (abs(tele.position.x - board_bot.position.x) + abs(tele.position.y - board_bot.position.y)))
+    
+    # Deteksi diamond di sekitar teleporter yang jauh jika diamond dekat kita atau base habis
+    def detectDiamondTeleporter(self, board_bot: GameObject, board: Board):
+        teleporters = self.findAllTeleporter(board_bot, board)
+        # Closest teleporter: 0, furthest: 1
+        farTeleporter = teleporters[1]
+        diamondCloseTeleporter = []
+        for diamond in board.diamonds:
+            if abs(diamond.position.x - farTeleporter.position.x) <= 3 and abs(diamond.position.y - farTeleporter.position.y) <= 3:
+                diamondCloseTeleporter.append(diamond)
+        return len(diamondCloseTeleporter) >= 3
+        # Hanya jika worth it, diamond > 3
+             
+    
+    #! GET DIRECTIONS
+    # Set direction and move, 2nd main function
     def get_directions(self,current_x, current_y, dest_x, dest_y):
         # bikin biar geraknya rada zigzag
         # cari jarak x dan y nya dulu
@@ -126,10 +203,21 @@ class attackBot(BaseLogic):
         props = board_bot.properties
         current_position = board_bot.position
         
+        max_travel_time = props.milliseconds_left # 1 detik buat jaga-jaga
+        distance_to_base = abs(props.base.x - current_position.x) + abs(props.base.y - current_position.y)
+        time_to_return = distance_to_base #sesuain disini kalau bot kita bisa gerak lbh cepet dr 1 detik
+        
+        # kalau mepet waktu, langsung balik ke base
+        if time_to_return >= max_travel_time:
+            print("time")
+            base = board_bot.properties.base
+            self.goal_position = base
+        
         # kalau diamond yang dimiliki sudah lebih dari 3 maka bot diarahkan balik ke base
-        if props.diamonds >=3:
+        elif props.diamonds >=3:
+            print("diamond minimal 3")
             # kalau pas jalan pulang ternyata ada diamond yang deket dan inventory belum penuh (sekalian ambil)
-            if(self.closestdiamonddist(board_bot,board)==1) and props.diamonds <5:
+            if(self.closestdiamonddist(board_bot,board)==2) and props.diamonds <5:
                 self.goal_position = self.closestdiamond(board_bot,board)
             else:
                 base = board_bot.properties.base
@@ -137,19 +225,29 @@ class attackBot(BaseLogic):
             
         # kalau masih kurang 3 akan cari diamond
         elif props.diamonds < 3:
-            # didahuluin cari yang ada di sekitar base dulu
+            print("diamond kurang")
             if self.cekdiamondbase(board_bot,board) and self.botaroundbase(board_bot):
+                print("diamond base")
                 diamond_list = self.diamondsaroundbase(board_bot,board)
-                self.goal_position = self.closestdiamondbase(board_bot, diamond_list)
+                self.goal_position = self.closestdiamondbase(board_bot,diamond_list)
             # kalau gak ada baru cari yang lebih jauh
             else:
-                # Cek dulu apakah ada bot yang memiliki diamonds lebih banyak dan jaraknya dekat
-                allBots = self.findAllBots(board_bot, board)
-                if (not self.chaseBots(board_bot, allBots, board)):
-                    self.goal_position = self.closestdiamond(board_bot,board)
-                else: 
-                    self.step+=1
-
+                # Prioritaskan red diamond jika lebih dekat dari pada diamond biasa
+                if self.closestreddiamonddist(board_bot, board) is not None and self.closestreddiamonddist(board_bot, board) < self.closestdiamonddist(board_bot, board):
+                    print("red diamond")
+                    self.goal_position = self.closestreddiamond(board_bot,board)
+                    
+                else:
+                    # Kalau terlalu jauh dan diamond nya sisa dikit, ke red button
+                    # Dengan kondisi dipastikan diamonds di sekitar base atau kita memang jauh, langsung jadikan prioritas untuk ke teleporter
+                    if self.detectDiamondTeleporter(board_bot, board) and not self.isPortal:
+                        self.goal_position = self.findAllTeleporter(board_bot, board)[0].position
+                        print("teleporter")
+                        self.isPortal = not self.isPortal
+                    else:
+                        print("diamond biru")
+                        self.goal_position = self.closestdiamond(board_bot, board)
+        
         if self.goal_position is not None:
             delta_x, delta_y = self.get_directions(
                 current_position.x,
@@ -157,8 +255,12 @@ class attackBot(BaseLogic):
                 self.goal_position.x,
                 self.goal_position.y,
             )
+        else: # Default jika goal_position kosong
+            delta_x, delta_y = self.get_directions(
+                current_position.x,
+                current_position.y,
+                board_bot.properties.base.x,
+                board_bot.properties.base.y
+            )
 
-        for obj in board.game_objects:
-            print(obj.type)
-        
         return delta_x, delta_y
---
+++ src/run.bat
@@ -0,0 +1,3 @@
+@echo off
+start cmd /c "python main.py --logic Padibot --email=padi@email.com --name=padi --password=123456 --team etimo"
+start cmd /c "python main.py --logic attackBot --email=attackbot@email.com --name=attackBot --password=123456 --team etimo"
